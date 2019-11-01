368ce7b6889ef77db9cfe85b8da52f61c4c41467
Allison-Silverpine <allison@silverpinesoftware.com>
2019-11-01 10:15:24 -0700

    Change to card images; Finish set validation logic

--- app/src/main/java/com/example/set/MainActivity.kt
+++ app/src/main/java/com/example/set/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.set
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var card1 : TextView
-    private lateinit var card2 : TextView
-    private lateinit var card3 : TextView
+    private lateinit var card1 : ImageView
+    private lateinit var card2 : ImageView
+    private lateinit var card3 : ImageView
     private lateinit var yesButton : Button
     private lateinit var noButton : Button
     private val styles = listOf(R.color.colorBlueBlack, R.color.colorSunflower, R.color.colorGreenery)
@@ -24,18 +26,25 @@ class MainActivity : AppCompatActivity() {
     private fun initializeBoard()
     {
         val random = Random()
-        val first = random.nextInt(styles.size-1) + 1
-        val second = random.nextInt(styles.size-1) + 1
-        val third = random.nextInt(styles.size-1) + 1
+        val first = random.nextInt(styles.size)
+        val second = random.nextInt(styles.size)
+        val third = random.nextInt(styles.size)
         card1 = findViewById(R.id.card1)
-        card1.setBackgroundResource(styles[first])
-        card1.tag = styles[first].toString()
+        card1.setImageResource(R.drawable.six_hollow_orange_rectangle)
+        card1.tag = "six_hollow_orange_rectangle"
+        //card1.setBackgroundResource(styles[first])
+        //card1.tag = styles[first].toString()
         card2= findViewById(R.id.card2)
-        card2.setBackgroundResource(styles[second])
-        card2.tag = styles[second].toString()
+        card2.setImageResource(R.drawable.one_solid_green_diamond)
+        card2.tag = "one_solid_green_diamond"
+        //card2.setBackgroundResource(styles[second])
+        //card2.tag = styles[second].toString()
         card3 = findViewById(R.id.card3)
-        card3.setBackgroundResource(styles[third])
-        card3.tag = styles[third].toString()
+        card3.setImageResource(R.drawable.three_striped_blue_circle)
+        card3.tag = "three_striped_blue_circle"
+        //card3.setBackgroundResource(styles[third])
+        //card3.tag = styles[third].toString()
+
         yesButton = findViewById(R.id.set_button)
         yesButton.setOnClickListener{ yesButton() }
         noButton = findViewById(R.id.not_set_button)
@@ -45,7 +54,7 @@ class MainActivity : AppCompatActivity() {
     private fun yesButton()
     {
         val resultText : TextView = findViewById(R.id.set_result)
-        if(isSet())
+        if(isSet(card1.tag.toString(), card2.tag.toString(), card3.tag.toString()))
         {
             resultText.text = "Correct!"
         }
@@ -56,7 +65,7 @@ class MainActivity : AppCompatActivity() {
     private fun noButton()
     {
         val resultText : TextView = findViewById(R.id.set_result)
-        if(isSet())
+        if(isSet(card1.tag.toString(), card2.tag.toString(), card3.tag.toString()))
         {
             resultText.text = "It's actually a set"
         }
@@ -64,9 +73,21 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun isSet() : Boolean
+    private fun isSet(card1: String, card2: String, card3: String) : Boolean
     {
-        return card1.tag.toString() == card2.tag.toString() && card2.tag.toString() == card3.tag.toString()
+        val card1Attrs = card1.split('_')
+        val card2Attrs = card2.split('_')
+        val card3Attrs = card3.split('_')
+        return validSet(card1Attrs[0], card2Attrs[0], card3Attrs[0]) &&
+                validSet(card1Attrs[1], card2Attrs[1], card3Attrs[1]) &&
+                validSet(card1Attrs[2], card2Attrs[2], card3Attrs[2]) &&
+                validSet(card1Attrs[3], card2Attrs[3], card3Attrs[3])
+    }
+
+    private fun validSet(attr1: String, attr2: String, attr3: String) : Boolean
+    {
+        return (attr1 == attr2 && attr2 == attr3) ||
+                (attr1 != attr2 && attr2 != attr3 && attr1 != attr3)
     }
 
 
