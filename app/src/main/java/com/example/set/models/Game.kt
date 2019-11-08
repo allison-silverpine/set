af2b897190f7bd22bcc414fd99901376b23c0c5a
Allison-Silverpine <allison@silverpinesoftware.com>
2019-11-08 15:35:47 -0800

    Start on board update

--- app/src/main/java/com/example/set/models/Game.kt
+++ app/src/main/java/com/example/set/models/Game.kt
@@ -5,6 +5,7 @@ class Game{
         private set
     var board: MutableList<String>
         private set
+    var gameWon = false
 
     init {
         board = mutableListOf()
@@ -35,6 +36,8 @@ class Game{
 
     fun isSet(card1: String, card2: String, card3: String) : Boolean
     {
+        if (!boardContainsCards(card1, card2, card3)) return false
+
         val card1Attrs = card1.split('_')
         val card2Attrs = card2.split('_')
         val card3Attrs = card3.split('_')
@@ -44,17 +47,33 @@ class Game{
                 validSet(card1Attrs[3], card2Attrs[3], card3Attrs[3])
     }
 
+    private fun boardContainsCards(card1: String, card2: String, card3: String) : Boolean {
+        return board.contains(card1) && board.contains(card2) && board.contains(card3)
+    }
+
     private fun validSet(attr1: String, attr2: String, attr3: String) : Boolean
     {
         return (attr1 == attr2 && attr2 == attr3) ||
                 (attr1 != attr2 && attr2 != attr3 && attr1 != attr3)
     }
 
-    fun makeGuess(card1: String, card2: String, card3: String){
-       //TODO
+    fun makeGuess(card1: String, card2: String, card3: String) : Boolean {
+        if (gameWon || !isSet(card1, card2, card3)) return false
+
+        val cards = listOf( card1, card2, card3)
+        if(deck.cards.isEmpty()){
+            cards.forEach { board.remove(it) }
+            if (!boardContainsSet())
+                gameWon = true
+        }
+        else{
+            updateBoard(cards)
+        }
+        return true
     }
 
-    fun boardContainsCards(card1: String, card2: String, card3: String) : Boolean {
-        return board.contains(card1) && board.contains(card2) && board.contains(card3)
+    private fun updateBoard(cards: List<String>){
+        //TODO
     }
+
 }
